e6545aeeb5c5ae574620d616ee1e67ee8dd76004
Real-Pixker <999patrickobama@gmail.com>
2024-03-25 14:58:24 +0300

    Check

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Amazon.com. Spend less. smile more.</title>
     <link rel="stylesheet" href="/clone_website/index.css">
+    <!-- Hey am checking out collaboration -->
 </head>
 
 <body>
